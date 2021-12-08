9670c0e69d79a5e130cf4aec680dc68fc512382f
Angel Valdez <ang_endless27@hotmail.com>
2021-12-08 11:21:54 -0600

    ang

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { LoginGenComponent } from './components/login-gen/login-gen.component';
 import { LoginFormComponent } from './components/login-form/login-form.component';
+import { ChatComponent } from './components/chat/chat.component';
+import { FriendsComponent } from './components/friends/friends.component';
+import { RequestComponent } from './components/request/request.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'login', component: LoginGenComponent, pathMatch: 'full'},
   {path: 'register', component: LoginGenComponent, pathMatch: 'full'},
+  {path:'chat/:ide',component:ChatComponent},
+  {path:'friends',component:FriendsComponent},
+  {path:'request',component:RequestComponent},
   {path: '**', redirectTo: 'home'},
 ];
 
---
+++ src/app/interfaces/Monami.ts
@@ -0,0 +1,3 @@
+export interface Monami{
+    ide:number; 
+}
